9100c1b5a262ef2da4bd1d03b6fd641210f714e2
Anthony Williams <anthony.williams.cs@gmail.com>
2019-04-26 22:27:36 -0600

    Output parsed int

--- FSharpFun/Program.fs
+++ FSharpFun/Program.fs
@@ -5,4 +5,8 @@ open System
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let input = stdin.ReadLine()
+    printfn "User entered: %s" input
+    let intVal = input |> int
+    printfn "int val: %i" intVal
     0 // return an integer exit code
